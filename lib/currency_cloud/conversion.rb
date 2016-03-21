84a476cf9662856453d9a84f866509140d01d236
Nitesh Varma <nitesh.varma@allerin.com>
2016-03-21 16:56:11 +0530

    Update conversion.rb

--- lib/currency_cloud/conversion.rb
+++ lib/currency_cloud/conversion.rb
@@ -1,8 +1,12 @@
 module CurrencyCloud
   class Conversion
     include CurrencyCloud::Resource
-
+    include CurrencyCloud::CustomerRate
+    
     resource :conversions
     actions :create, :retrieve, :find
+    
+    alias_attribute :client_sell_currency, :sell_currency
+    alias_attribute :client_buy_currency, :buy_currency
   end
 end
